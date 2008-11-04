b6bbbedf24d7428ef92aa11385b76c4ee96c819f
Rob <rob@tesla.local>
2008-11-04 14:20:37 -0800

    Froze rails

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :branch, "master"
 # servers (which is the default), you can specify the actual location
 # via the :deploy_to variable:
 set :deploy_to, "/var/www/#{application}"
-set :deploy_via, :remote_cache
+# set :deploy_via, :remote_cache
 
 # If you aren't using Subversion to manage your source code, specify
 # your SCM below:
